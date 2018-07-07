7ac093e123cc636e0dff92cf6e3f273ed55e3a5f
John Martin <johnny@iamjohnnym.com>
2018-07-07 00:14:54 -0700

    init

---
+++ bin/deskstrap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install Command Line Tools
+xcode-select --install
+
