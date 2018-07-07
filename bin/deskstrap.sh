ccfa6e84a155f983bb41a993a763a9734039dc39
John Martin <johnny@iamjohnnym.com>
2018-07-07 00:34:25 -0700

    installing xcode cli before brew

--- bin/deskstrap.sh
+++ bin/deskstrap.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 
-# Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
 # Install Command Line Tools
 xcode-select --install
 
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
