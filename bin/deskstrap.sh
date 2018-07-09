815f78da75ba012a285b5184b38154d0a7f6bb86
John Martin <johnny@iamjohnnym.com>
2018-07-08 19:37:51 -0700

    Install brew things.

--- bin/deskstrap.sh
+++ bin/deskstrap.sh
@@ -6,3 +6,9 @@ xcode-select --install
 
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Symlink Brewfile
+ln -s ./brew/Brewfile ~/Brewfile
+
+# Install applications with Brew
+brew install
